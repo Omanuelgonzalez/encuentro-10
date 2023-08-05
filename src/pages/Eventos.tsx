141a688293907ea0669aac6ecb47d0e9fcac20a7
Osman <est.Ogonxalezx0201@uml.edu.ni>
2023-08-05 14:46:26 -0600

    subir

--- src/pages/Eventos.tsx
+++ src/pages/Eventos.tsx
@@ -1,10 +1,28 @@
-import { Title } from "../components/Title"
-
+import { useEffect, useState } from "react";
+import { Title } from "../components/Title";
 
 export const Eventos = () => {
+const [data, setData] = useState ([]);
+  useEffect(() => {
+   fetch('https://jsonplaceholder.typicode.com/posts/1/posts')
+     .then((response) => response.json())
+     .then((datos) => setData(datos));
+   },[])
+
   return (
     <>
-      <Title titulo="Bienvenido!" />
+      <Title titulo="Lista de eventos disponibles!" />
+
+<ul>
+  {data.map((item: any) => (
+<li  className= 'mb-16 relative'key={item.id}>
+<h1 className={'text-2xl font-bold uppercase text-green-500'}>{item.title}</h1>
+<p className='text-lg italic '>{item.body}</p>
+
+<div className='absolute w-1/2 translate-x-1/2  border-b border-red-500 -botton-10'></div>
+</li>
+  ))}
+</ul>
     </>
   )
 }
